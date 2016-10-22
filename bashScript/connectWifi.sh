aadd92d8a4c05474983990773aeae467df559165
Robert Marver <ram16@cs.washington.edu>
2016-10-22 12:32:54 -0400

    script should work?

--- bashScript/connectWifi.sh
+++ bashScript/connectWifi.sh
@@ -7,10 +7,13 @@ read tagID
 # Call Caroline's and Alec's python code for decryption.
 # <wifi_name>_<wifi_password>
 ### python <name_of_function> $tagID | wifiInfo
-WIFIINFO="Harvard_University__Fishface"
-WIFIINPUT=($(echo $WIFIINFO | tr "__" "\n"))
-USERNAME=${WIFIINPUT[0]} ${WIFIINPUT[1]}
-PASSWORD=${WIFIINPUT[2]}
+DECODED="Harvard Secure_Vandoren7942"
+IFS="_"; WIFIINPUT=($DECODED); unset IFS;
+USERNAME=${WIFIINPUT[0]}
+PASSWORD=${WIFIINPUT[1]}
 echo "Wifi username: $USERNAME"
 echo "Wifi password: $PASSWORD"
-airport -s | grep $USERNAME
+airport -s | grep "$USERNAME"
+IFS="_"
+networksetup -setairportnetwork en0 $USERNAME $PASSWORD
+unset IFS
